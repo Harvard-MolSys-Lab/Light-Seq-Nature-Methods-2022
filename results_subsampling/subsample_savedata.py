b4e2695820af340144480cb33954ce906902cdb6
Ninning <ninningliu@gmail.com>
2021-10-25 16:53:25 -0400

    Update subsample_savedata.py

--- results_subsampling/subsample_savedata.py
+++ results_subsampling/subsample_savedata.py
@@ -13,17 +13,15 @@ bamfile = sorted(globBAMfiles(type="dedup"))
 #print("Deduped and indexed bam files: ")
 #print(bamfile)
 
+## get umis and fraction values ##
 umis = []
+fracs = []
 for file in bamfile:
     samfile = pysam.AlignmentFile(file, "rb")
     umis.append(samfile.mapped)
+    fracs.append(file.split("_")[1])
 
-## get fraction values ##
-temp = []
-for file in bamfile:
-    temp.append(file.split("_")[1])
-    fracs = sorted(list(set(temp)))
-
+fracs = sorted(list(set(fracs)))
 umis_arr = np.array(umis)
 
 #reshape array to match dataframe
