b64bc124ec55693c187f849cdda410d3c16c5a83
Ninning <ninningliu@gmail.com>
2021-11-07 23:23:14 -0500

    Moved setnumbersDF function
    moved setnumbersDF functions and set the dictionary indexing to be more robust.

--- cellmixing_1_parsegenes.py
+++ cellmixing_1_parsegenes.py
@@ -1,25 +1,26 @@
-#Changed to count ALL genes using gene_id. sometimes two ENSEMBL Ids can have the same gene name.
-#NL
+# Changed to count ALL genes using gene_id. sometimes two ENSEMBL Ids can have the same gene name.
+# NL
 import os
 import glob
-import pysam
 import csv
 from collections import defaultdict
+
 import pandas as pd
+import pysam
 
-import setNumbersDF
+from utils import setNumbersDF
 
-#Seq files are not hosted on the github platform due to size limitations.
-#This script is meant to be run on the .bam files for the mouse human mixing experimens (436, 438)
-#Set the folder for the .bam files that have been deduped from UMI tools.
-#Files should have *dedup.bam. Script also uses pysam which requires an indexed ".bam.bai" file.
-#So run the samtools indexer in the folder as well.
+# Seq files are not hosted on the github platform due to size limitations.
+# This script is meant to be run on the .bam files for the mouse human mixing experimens (436, 438)
+# Set the folder for the .bam files that have been deduped from UMI tools.
+# Files should have *dedup.bam. Script also uses pysam which requires an indexed ".bam.bai" file.
+# So run the samtools indexer in the folder as well.
 
 PATH_BAMFILES = "436_438_outFiles_deduped/"
 PATH_GENES = "results_cellmixing_genes/"
 PATH_NUMBERS = "results_cellmixing_numbers/"
 
-BAMFILES = sorted(glob.glob('%s*_dedup.bam' %PATH_BAMFILES))
+BAMFILES = sorted(glob.glob('%s*_dedup.bam' % PATH_BAMFILES))
 DICTIONARY = "genes_dict_combined/genesDict_types_lengths_combined.csv"
 
 B1 = "GTTAGG"
@@ -30,6 +31,7 @@ if os.path.isdir("results_cellmixing_genes") is False:
 
 if os.path.isdir("results_cellmixing_numbers") is False:
 	os.mkdir("results_cellmixing_numbers")
+
 
 class Parser(object):
 	"""
@@ -42,12 +44,12 @@ class Parser(object):
 		self.fname_short = self.filename.split("_")[0]
 		self.reads = READS
 		self.parseBC()
-		self.species = []
+		self.species = '_species_'
 		
 	def parseBC(self):
-		self.bc1=[]
-		self.bc2=[]
-		for idx,read in enumerate(self.reads):
+		self.bc1 = []
+		self.bc2 = []
+		for idx, read in enumerate(self.reads):
 			bc = read.query_name.split("_")[1]
 			if B1 in bc:
 				self.bc1.append(idx)
@@ -57,26 +59,28 @@ class Parser(object):
 		return self.bc1, self.bc2
 				
 	def geneCount(self, bc_set):
-		genecounts = {}
 		genecounts = defaultdict(lambda: 0)
 
-		for _,read in enumerate(bc_set):
+		for _, read in enumerate(bc_set):
 			gene_ID = self.reads[read].get_tag("XT")
-			genecounts[gene_ID]+=1
+			genecounts[gene_ID] += 1
 		
 		return genecounts
 	
-	#Determines whether you entered Mouse or Human Reads
-	#Will also return a string for later use.
+	# Determines whether you entered Mouse or Human Reads
+	# Will also return a string for later use.
 	def setDF(self):
 		df = pd.read_csv(DICTIONARY, index_col=0)
-		df["gene_count"]=0
+		df["gene_count"] = 0
+		m_index = list(map(lambda name: "ENSMUSG" in name, list(df.index))).index(True)  # Finds first instance of a mouse gene
+
 		if "ENSG" in self.reads[0].get_tag("XT"):
-			df = df[:60612]
+			df = df[:m_index]
 			self.species = "_Human_"
 		elif "ENSMUSG" in self.reads[0].get_tag("XT"):
-			df = df[60612:]
+			df = df[m_index:]
 			self.species = "_Mouse_"
+			
 		return df
 
 	def geneCountAll(self, bc_set):
@@ -85,40 +89,40 @@ class Parser(object):
 
 		for item in genecounts:
 			count = genecounts.get(item)
-			df.loc[item,"gene_count"] = count 
-		
+			df.loc[item, "gene_count"] = count
+
 		return df
 
-#Find first instance of mChr1
+
+# Find first instance of mChr1
 def indexHumanMouse(samfile):
-	temp_index = []
 	temp_index = samfile.references.index('mchr1')
 
 	gindex_human = range(0, temp_index)
-	gindex_mouse = range(temp_index , len(samfile.references))
+	gindex_mouse = range(temp_index, len(samfile.references))
 	
 	return gindex_human, gindex_mouse
 
-#Parse out the reads for the entire bamfile
-#Each item is an alignment file
+
 def parseReads(gindex_human, gindex_mouse, samfile):
-	
+	# Parse out the reads for the entire bamfile
+	# Each item is an alignment file
 	HUMAN_READS = []
 	for idx in gindex_human:
-		iter = samfile.fetch(tid = idx, until_eof=True)
+		iter = samfile.fetch(tid=idx, until_eof=True)
 		for reads in iter:
 			HUMAN_READS.append(reads)
 			
 	MOUSE_READS = []
 	for idx in gindex_mouse:
-		iter = samfile.fetch(tid = idx, until_eof=True)
+		iter = samfile.fetch(tid=idx, until_eof=True)
 		for reads in iter:
 			MOUSE_READS.append(reads)
 	
 	return HUMAN_READS, MOUSE_READS
 
-def main():
 
+def main():
 	for bamfile in BAMFILES:
 
 		samfile = pysam.AlignmentFile(bamfile, "rb")
@@ -141,14 +145,12 @@ def main():
 		# 	PATH_GENES + MOUSE_MAPS.fname_short + MOUSE_MAPS.species + "bc2_AllCounts.csv"
 		# 	)
 
-		fname_short = HUMAN_MAPS.fname_short #Either maps name is fine
+		fname_short = HUMAN_MAPS.fname_short  # Either maps name is fine
 		numbers_df = setNumbersDF.setDF(HUMAN_MAPS, MOUSE_MAPS, samfile.mapped)
 
 		numbers_df.to_csv(PATH_NUMBERS + fname_short + "_NUMBERS.csv")
 		print("Finished with file: " + fname_short)
 
+
 if __name__ == '__main__':
 	main()
-
-
-
