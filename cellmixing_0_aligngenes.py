b287308b600bfead78993b47d8b381ef063f5732
Ninning <ninningliu@gmail.com>
2021-11-08 15:54:20 -0500

    Adjusted spacing, more readable

--- cellmixing_0_aligngenes.py
+++ cellmixing_0_aligngenes.py
@@ -2,7 +2,7 @@ import os
 import glob
 
 PATH_BAMFILES = "outFiles_genome_alignPerGene/"
-FILE_GTF      = "/Users/ninning/o2Download/STAR_mergedIndex/gencode.v38andvM27.annotation.gff3"
+FILE_GTF      = "gencode.v38andvM27.annotation.gff3"
 BAMFILES      = sorted(glob.glob('%s*sortedByCoord.out.bam' % PATH_BAMFILES))
 PATH_OUTFILE  = "436_438_outFiles_deduped/"
 
@@ -11,73 +11,71 @@ OUTPUT_STATS = False
 
 
 def getFileNamesAll(files):
-	filenames = [file.split('/')[-1].split('.bam')[0] for file in files]
-	print('Getting file names: ')
-	print(filenames)
+    filenames = [file.split('/')[-1].split('.bam')[0] for file in files]
+    print('Getting file names: ')
+    print(filenames)
 
 
 def getFileName(bamfile):
-	filename = (bamfile.split('/')[-1].split('.bam')[0])
-	return filename
+    filename = (bamfile.split('/')[-1].split('.bam')[0])
+    return filename
 
 
 def featureCounts(bamfile):
-	filename = getFileName(bamfile)
-	os.system('featureCounts -a %s -o %s -R BAM %s' % (
-		FILE_GTF,
-		PATH_BAMFILES + filename + '.gene_assigned',
-		bamfile)
-	)
+    filename = getFileName(bamfile)
+    os.system('featureCounts -a %s -o %s -R BAM %s' % (
+        FILE_GTF,
+        PATH_BAMFILES + filename + '.gene_assigned',
+        bamfile))
 
 
 def sortIndex(bamfile):
-	fcount_file = bamfile + '.featureCounts.bam'
-	filename = getFileName(bamfile)
+    fcount_file = bamfile + '.featureCounts.bam'
+    filename = getFileName(bamfile)
 
-	print("Sorting File: %s" % fcount_file)
-	os.system('samtools sort %s -o %s' % (
-		fcount_file, PATH_BAMFILES + filename + '_sorted.bam')
-	)
+    print("Sorting File: %s" % fcount_file)
+    os.system('samtools sort %s -o %s' % (
+        fcount_file, PATH_BAMFILES + filename + '_sorted.bam'))
 
-	print("Indexing Sorted File: %s" % (PATH_BAMFILES + filename + '_sorted.bam'))
-	os.system('samtools index %s' % (
-		PATH_BAMFILES + filename + '_sorted.bam')
-	)
+    print("Indexing Sorted File: %s" % (
+        PATH_BAMFILES + filename + '_sorted.bam'))
+    os.system('samtools index %s' % (
+        PATH_BAMFILES + filename + '_sorted.bam'))
 
 
 def umiDedup(bamfile):
-	# Make sure you have the sorted.bam and index file present in the directory
-	filename = getFileName(bamfile)
+    # Make sure you have the sorted.bam and index file present in the directory
+    filename = getFileName(bamfile)
 
-	if OUTPUT_STATS:
-		umistr = 'umi_tools dedup --per-gene --gene-tag=XT --assigned-status-tag=XS -I %s --output-stats=%s -S %s --log %s' % (
-			PATH_BAMFILES + filename + '_sorted.bam', 
-			PATH_OUTFILE  + filename + '_dedupstats', 
-			PATH_OUTFILE  + filename + '_dedup.bam', 
-			PATH_OUTFILE  + filename + '_deduplog.log')
+    if OUTPUT_STATS:
+        umistr = 'umi_tools dedup --per-gene --gene-tag=XT --assigned-status-tag=XS -I %s --output-stats=%s -S %s --log %s' % (
+            PATH_BAMFILES + filename + '_sorted.bam', 
+            PATH_OUTFILE  + filename + '_dedupstats', 
+            PATH_OUTFILE  + filename + '_dedup.bam', 
+            PATH_OUTFILE  + filename + '_deduplog.log')
 
-		print('\n' + 'Running: ' + umistr + '\n')
-		os.system(umistr)
+        print('\n' + 'Running: ' + umistr + '\n')
+        os.system(umistr)
 
-	else:
-		umistr = 'umi_tools dedup --per-gene --gene-tag=XT --assigned-status-tag=XS -I %s -S %s --log %s' % (
-			PATH_BAMFILES + filename + '_sorted.bam', 
-			PATH_OUTFILE  + filename + '_dedup.bam', 
-			PATH_OUTFILE  + filename + '_deduplog.log')
-		
-		print('\n' + 'Running: ' + umistr + '\n')
-		os.system(umistr)
+    else:
+        umistr = 'umi_tools dedup --per-gene --gene-tag=XT --assigned-status-tag=XS -I %s -S %s --log %s' % (
+            PATH_BAMFILES + filename + '_sorted.bam', 
+            PATH_OUTFILE  + filename + '_dedup.bam', 
+            PATH_OUTFILE  + filename + '_deduplog.log')
+        
+        print('\n' + 'Running: ' + umistr + '\n')
+        os.system(umistr)
 
 
 def main():
-	getFileNamesAll(BAMFILES)
+    getFileNamesAll(BAMFILES)
 
-	# Run analysis one file at a time
-	for bamfile in BAMFILES:
-		featureCounts(bamfile)
-		sortIndex(bamfile)
-		umiDedup(bamfile)
+    # Run analysis one file at a time
+    for bamfile in BAMFILES:
+        featureCounts(bamfile)
+        sortIndex(bamfile)
+        umiDedup(bamfile)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- cellmixing_1_parsegenes.py
+++ cellmixing_1_parsegenes.py
@@ -1,4 +1,4 @@
-# Changed to count ALL genes using gene_id. sometimes two ENSEMBL Ids can have the same gene name.
+# Changed to count ALL genes using gene_id. 
 # NL
 import os
 import glob
@@ -11,10 +11,13 @@ import pysam
 from utils import setNumbersDF
 
 # Seq files are not hosted on the github platform due to size limitations.
-# This script is meant to be run on the .bam files for the mouse human mixing experimens (436, 438)
+# This script is meant to be run on the .bam files 
+# for the mouse human mixing experimens (436, 438).
+
 # Set the folder for the .bam files that have been deduped from UMI tools.
-# Files should have *dedup.bam. Script also uses pysam which requires an indexed ".bam.bai" file.
-# So run the samtools indexer in the folder as well.
+# Files should have *dedup.bam. Script also uses pysam which requires an 
+# indexed ".bam.bai" file.
+
 
 PATH_BAMFILES = "436_438_outFiles_deduped/"
 PATH_GENES = "results_cellmixing_genes/"
@@ -27,130 +30,145 @@ B1 = "GTTAGG"
 B2 = "TATGGA"
 
 if os.path.isdir("results_cellmixing_genes") is False:
-	os.mkdir("results_cellmixing_genes")
+    os.mkdir("results_cellmixing_genes")
 
 if os.path.isdir("results_cellmixing_numbers") is False:
-	os.mkdir("results_cellmixing_numbers")
+    os.mkdir("results_cellmixing_numbers")
 
 
 class Parser(object):
-	"""
-	Parse out BC1 or BC2 for sequences
-	Also sets the dataframe to be ENSG or ENSMUSG depending on the input reads
-	"""
-	def __init__(self, file, READS):
-		self.file = file
-		self.filename = (file.split('/')[-1])
-		self.fname_short = self.filename.split("_")[0]
-		self.reads = READS
-		self.parseBC()
-		self.species = '_species_'
-		
-	def parseBC(self):
-		self.bc1 = []
-		self.bc2 = []
-		for idx, read in enumerate(self.reads):
-			bc = read.query_name.split("_")[1]
-			if B1 in bc:
-				self.bc1.append(idx)
-			if B2 in bc:
-				self.bc2.append(idx)
-		
-		return self.bc1, self.bc2
-				
-	def geneCount(self, bc_set):
-		genecounts = defaultdict(lambda: 0)
+    """
+    Parse out BC1 or BC2 for sequences
+    Also sets the dataframe to be ENSG or ENSMUSG depending on the input reads
+    """
+    def __init__(self, file, READS):
+        self.file = file
+        self.filename = (file.split('/')[-1])
+        self.fname_short = self.filename.split("_")[0]
+        self.reads = READS
+        self.parseBC()
+        self.species = '_species_'
+        
+    def parseBC(self):
+        self.bc1 = []
+        self.bc2 = []
+        for idx, read in enumerate(self.reads):
+            bc = read.query_name.split("_")[1]
+            if B1 in bc:
+                self.bc1.append(idx)
+            if B2 in bc:
+                self.bc2.append(idx)
+        
+        return self.bc1, self.bc2
+                
+    def geneCount(self, bc_set):
+        genecounts = defaultdict(lambda: 0)
 
-		for _, read in enumerate(bc_set):
-			gene_ID = self.reads[read].get_tag("XT")
-			genecounts[gene_ID] += 1
-		
-		return genecounts
-	
-	# Determines whether you entered Mouse or Human Reads
-	# Will also return a string for later use.
-	def setDF(self):
-		df = pd.read_csv(DICTIONARY, index_col=0)
-		df["gene_count"] = 0
-		m_index = list(map(lambda name: "ENSMUSG" in name, list(df.index))).index(True)  # Finds first instance of a mouse gene
+        for _, read in enumerate(bc_set):
+            gene_ID = self.reads[read].get_tag("XT")
+            genecounts[gene_ID] += 1
+        
+        return genecounts
+    
+    # Determines whether you entered Mouse or Human Reads
+    # Will also return a string for later use.
+    def setDF(self):
+        df = pd.read_csv(DICTIONARY, index_col=0)
+        df["gene_count"] = 0
 
-		if "ENSG" in self.reads[0].get_tag("XT"):
-			df = df[:m_index]
-			self.species = "_Human_"
-		elif "ENSMUSG" in self.reads[0].get_tag("XT"):
-			df = df[m_index:]
-			self.species = "_Mouse_"
-			
-		return df
+        # Finds first instance of a mouse gene
+        m_index = list(map(
+            lambda name: "ENSMUSG" in name, list(df.index))
+            ).index(True)
 
-	def geneCountAll(self, bc_set):
-		genecounts = self.geneCount(bc_set)
-		df = self.setDF()
+        if "ENSG" in self.reads[0].get_tag("XT"):
+            df = df[:m_index]
+            self.species = "_Human_"
+        elif "ENSMUSG" in self.reads[0].get_tag("XT"):
+            df = df[m_index:]
+            self.species = "_Mouse_"
+            
+        return df
 
-		for item in genecounts:
-			count = genecounts.get(item)
-			df.loc[item, "gene_count"] = count
+    def geneCountAll(self, bc_set):
+        genecounts = self.geneCount(bc_set)
+        df = self.setDF()
 
-		return df
+        for item in genecounts:
+            count = genecounts.get(item)
+            df.loc[item, "gene_count"] = count
+
+        return df
 
 
 # Find first instance of mChr1
 def indexHumanMouse(samfile):
-	temp_index = samfile.references.index('mchr1')
+    temp_index = samfile.references.index('mchr1')
 
-	gindex_human = range(0, temp_index)
-	gindex_mouse = range(temp_index, len(samfile.references))
-	
-	return gindex_human, gindex_mouse
+    gindex_human = range(0, temp_index)
+    gindex_mouse = range(temp_index, len(samfile.references))
+    
+    return gindex_human, gindex_mouse
 
 
 def parseReads(gindex_human, gindex_mouse, samfile):
-	# Parse out the reads for the entire bamfile
-	# Each item is an alignment file
-	HUMAN_READS = []
-	for idx in gindex_human:
-		iter = samfile.fetch(tid=idx, until_eof=True)
-		for reads in iter:
-			HUMAN_READS.append(reads)
-			
-	MOUSE_READS = []
-	for idx in gindex_mouse:
-		iter = samfile.fetch(tid=idx, until_eof=True)
-		for reads in iter:
-			MOUSE_READS.append(reads)
-	
-	return HUMAN_READS, MOUSE_READS
+    # Parse out the reads for the entire bamfile
+    # Each item is an alignment file
+    HUMAN_READS = []
+    for idx in gindex_human:
+        iter = samfile.fetch(tid=idx, until_eof=True)
+        for reads in iter:
+            HUMAN_READS.append(reads)
+            
+    MOUSE_READS = []
+    for idx in gindex_mouse:
+        iter = samfile.fetch(tid=idx, until_eof=True)
+        for reads in iter:
+            MOUSE_READS.append(reads)
+    
+    return HUMAN_READS, MOUSE_READS
 
 
 def main():
-	for bamfile in BAMFILES:
+    for bamfile in BAMFILES:
 
-		samfile = pysam.AlignmentFile(bamfile, "rb")
-		gindex_human, gindex_mouse = indexHumanMouse(samfile)
-		HUMAN_READS, MOUSE_READS = parseReads(gindex_human, gindex_mouse, samfile)
+        samfile = pysam.AlignmentFile(bamfile, "rb")
+        gindex_human, gindex_mouse = indexHumanMouse(samfile)
+        
+        HUMAN_READS, MOUSE_READS = parseReads(
+            gindex_human, gindex_mouse, samfile)
 
-		HUMAN_MAPS = Parser(bamfile, HUMAN_READS)
-		MOUSE_MAPS = Parser(bamfile, MOUSE_READS)
+        HUMAN_MAPS = Parser(bamfile, HUMAN_READS)
+        MOUSE_MAPS = Parser(bamfile, MOUSE_READS)
 
-		# HUMAN_MAPS.geneCountAll(HUMAN_MAPS.bc1).to_csv(
-		# 	PATH_GENES + HUMAN_MAPS.fname_short + HUMAN_MAPS.species + "bc1_AllCounts.csv"
-		# 	)
-		HUMAN_MAPS.geneCountAll(HUMAN_MAPS.bc2).to_csv(
-			PATH_GENES + HUMAN_MAPS.fname_short + HUMAN_MAPS.species + "bc2_AllCounts.csv"
-			)
-		MOUSE_MAPS.geneCountAll(MOUSE_MAPS.bc1).to_csv(
-			PATH_GENES + MOUSE_MAPS.fname_short + MOUSE_MAPS.species + "bc1_AllCounts.csv"
-			)
-		# MOUSE_MAPS.geneCountAll(MOUSE_MAPS.bc2).to_csv(
-		# 	PATH_GENES + MOUSE_MAPS.fname_short + MOUSE_MAPS.species + "bc2_AllCounts.csv"
-		# 	)
+        # HUMAN_MAPS.geneCountAll(HUMAN_MAPS.bc1).to_csv(
+        #   PATH_GENES 
+        #   + HUMAN_MAPS.fname_short + HUMAN_MAPS.species + "bc1_AllCounts.csv")
 
-		fname_short = HUMAN_MAPS.fname_short  # Either maps name is fine
-		numbers_df = setNumbersDF.setDF(HUMAN_MAPS, MOUSE_MAPS, samfile.mapped)
+        HUMAN_MAPS.geneCountAll(HUMAN_MAPS.bc2).to_csv(
+            PATH_GENES 
+            + HUMAN_MAPS.fname_short 
+            + HUMAN_MAPS.species 
+            + "bc2_AllCounts.csv"
+            )
+        
+        MOUSE_MAPS.geneCountAll(MOUSE_MAPS.bc1).to_csv(
+            PATH_GENES 
+            + MOUSE_MAPS.fname_short 
+            + MOUSE_MAPS.species 
+            + "bc1_AllCounts.csv"
+            )
+        
+        #MOUSE_MAPS.geneCountAll(MOUSE_MAPS.bc2).to_csv(
+        #   PATH_GENES 
+        #   + MOUSE_MAPS.fname_short + MOUSE_MAPS.species + "bc2_AllCounts.csv")
 
-		numbers_df.to_csv(PATH_NUMBERS + fname_short + "_NUMBERS.csv")
-		print("Finished with file: " + fname_short)
+        fname_short = HUMAN_MAPS.fname_short  # Either maps name is fine
+        numbers_df = setNumbersDF.setDF(HUMAN_MAPS, MOUSE_MAPS, samfile.mapped)
+
+        numbers_df.to_csv(PATH_NUMBERS + fname_short + "_NUMBERS.csv")
+        print("Finished with file: " + fname_short)
 
 
 if __name__ == '__main__':
-	main()
+    main()
