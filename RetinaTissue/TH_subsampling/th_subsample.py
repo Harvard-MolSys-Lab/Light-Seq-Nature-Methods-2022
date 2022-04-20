83ce0813e1cb1588fb43fe2b3cd6b0ceb1a55fe7
Ninning <ninningliu@gmail.com>
2022-04-20 17:23:37 -0400

    Update th_subsample.py

--- RetinaTissue/TH_subsampling/th_subsample.py
+++ RetinaTissue/TH_subsampling/th_subsample.py
@@ -9,7 +9,7 @@ import numpy as np
 import pysam
 
 FRAC = np.array(
-	[0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.9]
+    [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.9]
 )
 SIMS = 5
 OUTFOLDER = 'simOut/'
@@ -18,26 +18,26 @@ samfile = pysam.AlignmentFile("TLS23A_Sorted.bam", "rb")
 
 
 def subsim(frac, sim):
-	subsamfile = pysam.AlignmentFile(
-		"%ssubsampled_F%s_S%s.bam" % (OUTFOLDER, str(frac), str(sim)), 
-		"wb", 
-		template=samfile
-	)
-	
-	for read in samfile.fetch():
-		if random.random()<=frac:
-			subsamfile.write(read)
-	
-	subsamfile.close()
+    subsamfile = pysam.AlignmentFile(
+        "%ssubsampled_F%s_S%s.bam" % (OUTFOLDER, str(frac), str(sim)), 
+        "wb", 
+        template=samfile
+    )
+    
+    for read in samfile.fetch():
+        if random.random()<=frac:
+            subsamfile.write(read)
+    
+    subsamfile.close()
 
 
 def main():
-	for frac in FRAC:
-		print("Simulating FRAC: %s" % str(frac))
-		for sim in range(SIMS):
-			print("Simulating SIM: %s" % str(sim))
-			subsim(frac, sim)
+    for frac in FRAC:
+        print("Simulating FRAC: %s" % str(frac))
+        for sim in range(SIMS):
+            print("Simulating SIM: %s" % str(sim))
+            subsim(frac, sim)
 
 
 if __name__ == '__main__':
-	main()
+    main()
